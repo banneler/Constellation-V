fc053b7f1963de803f18f09dd2e673d89401b76e
banneler <banneler55@gmail.com>
2025-07-28 17:37:51 -0500

    Update shared_constants.js

--- js/shared_constants.js
+++ js/shared_constants.js
@@ -1,8 +1,7 @@
 // --- SHARED CONSTANTS AND FUNCTIONS ---
 
 export const SUPABASE_URL = "https://pjxcciepfypzrfmlfchj.supabase.co";
-export const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeGNjaWVwZnlwenJmbWxmY2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMTU4NDQsImV4cCI6MjA2NzY5MTg0NH0.m_jyE0e4QFevI-mGJHYlGmA12lXf8XoMDoiljUav79c";
+export const SUPABASE_ANON_KEY = "sb_publishable_vu_sGc6BTgY1Uqr20ditww_4VqqMLzO";
 
 export const themes = ["dark", "light", "green"];
 
