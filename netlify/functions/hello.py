3b01c9bcc01af758d5987ec4d9223b649ae3828c
banneler <banneler55@gmail.com>
2025-07-28 14:42:47 -0500

    Delete netlify/functions directory

--- netlify/functions/hello.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def handler(event, context):
-    print("Hello function invoked")
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Hello from your Netlify function!"})
-    }
