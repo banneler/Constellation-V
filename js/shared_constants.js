993ddcafb57e4d6d1d7bcca70aadb4398e1e8ba1
banneler <banneler55@gmail.com>
2025-08-02 16:25:49 -0500

    Update shared_constants.js

--- js/shared_constants.js
+++ js/shared_constants.js
@@ -3,7 +3,7 @@
 // --- SHARED CONSTANTS AND FUNCTIONS ---
 
 export const SUPABASE_URL = "https://pjxcciepfypzrfmlfchj.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzIΙNiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeGNjaWVwZnlwenJmbWxmY2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMTU4NDQsImV4cCI6MjA2NzY5MTg0NH0.m_jyE0e4QFevI-mGJHYlGmA12lXf8XoMDoiljUav79c";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeGNjaWVwZnlwenJmbWxmY2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMTU4NDQsImV4cCI6MjA2NzY5MTg0NH0.m_jyE0e4QFevI-mGJHYlGmA12lXf8XoMDoiljUav79c";
 
 export const themes = ["dark", "light", "green", "blue", "corporate"];
 
@@ -353,3 +353,4 @@ export async function loadSVGs() {
     }
   }
 }
+
