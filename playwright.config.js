017d5c1c92a2db7a11dcecc15f925e257cc5924f
banneler <banneler55@gmail.com>
2025-08-03 17:07:40 -0500

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,24 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. Your GitHub repo name is Constellation-V-main-9, so the GitHub Pages URL will follow this pattern. */
+    baseURL: 'https://banneler.github.io/Constellation-V-main-9',
+
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
